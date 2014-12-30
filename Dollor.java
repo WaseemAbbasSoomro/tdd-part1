7c99bc8c7689bd7aeb44c05f222e39e00aa1f943
WaseemAbbasSoomro <waseem.soomro@globalnorthstar.com>
2014-12-30 22:56:19 +0500

    Initialized amount field from constructor and multiply amount with multiplier.

--- Dollor.java
+++ Dollor.java
@@ -12,10 +12,11 @@ public class Dollor {
 	int amount;
 	
 	public Dollor( int amount ){
+		this.amount = amount;
 	}
 	
 	public void times( int multiplier ){
-		amount = 5 * 2;
+		amount *= multiplier;
 	}
 	
 }
