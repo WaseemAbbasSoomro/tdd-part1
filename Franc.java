5900aee580d95e14b9988ebe6af25153fbf4f75a
WaseemAbbasSoomro <waseem.soomro@globalnorthstar.com>
2014-12-31 01:33:06 +0500

    Introduces Franc class.

---
+++ Franc.java
@@ -0,0 +1,23 @@
+package sibisoft;
+
+/**
+ * @author Waseem Abbas
+ *
+ */
+public class Franc {
+
+private int amount;
+	
+	public Franc( int amount ){
+		this.amount = amount;
+	}
+	
+	public Franc times( int multiplier ){
+		return new Franc( amount * multiplier );
+	}
+	
+	public boolean equals( Object obj ){
+		Franc franc = (Franc) obj;
+		return amount == franc.amount;
+	}
+}
--- MoneyTest.java
+++ MoneyTest.java
@@ -15,9 +15,10 @@ public class MoneyTest {
 	}
 	
 	@Test
-	public void testEquality(){
-		assertTrue(new Dollor( 5 ).equals( new Dollor( 5 )));
-		assertTrue(new Dollor( 5 ).equals( new Dollor( 6 )));
+	public void testFrancMultiplication(){
+		Franc five = new Franc( 5 );
+		assertEquals(new Franc(10), five.times( 2 ) );
+		assertEquals( new Franc( 15 ), five.times( 3 ) );
 	}
 	
 }
