29a0b413ac37e66905713b9843bcebc4437bf8ef
WaseemAbbasSoomro <waseem.soomro@globalnorthstar.com>
2014-12-30 21:43:55 +0500

    First Commit

---
+++ Dollor.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package sibisoft;
+
+/**
+ * @author Waseem Abbas
+ *
+ */
+public class Dollor {
+
+	public int amount;
+	
+	public Dollor( int amount ){
+	}
+	
+	public void times( int multiplier ){
+	}
+	
+}
---
+++ MoneyTest.java
@@ -0,0 +1,16 @@
+package sibisoft;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class MoneyTest {
+	
+	@Test
+	public void testMultiplication(){
+		Dollor five = new Dollor( 5 );
+		five.times(2);
+		assertEquals(10, five.amount);
+	}
+	
+}
