fc8fb2ce867f224175aad09b3e933babed94ee10
WaseemAbbasSoomro <waseem.soomro@globalnorthstar.com>
2015-01-01 01:01:29 +0500

    Remove testFrancMultiplication method

--- MoneyTest.java
+++ MoneyTest.java
@@ -14,12 +14,12 @@ public class MoneyTest {
 		assertEquals( Money.dollor( 15 ), five.times( 3 ) );
 	}
 	
-	@Test
-	public void testFrancMultiplication(){
+	//@Test
+	/*public void testFrancMultiplication(){
 		Money five = Money.franc( 5 );
 		assertEquals(Money.franc(10), five.times( 2 ) );
 		assertEquals( Money.franc( 15 ), five.times( 3 ) );
-	}
+	}*/
 	
 	@Test
 	public void testEquality(){
